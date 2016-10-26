9aafb9402116ae8700c128d732e577559c08ccc8
imad.hashmi <imad.hashmi@rubik.com.au>
2016-10-26 12:47:02 +1100

    fixed some display views

--- AmountToText.Web/Scripts/App/DashboardController.js
+++ AmountToText.Web/Scripts/App/DashboardController.js
@@ -3,14 +3,22 @@
     $scope.ResultAmount = null;
     $scope.ErrorMessage = "";
 
+    $scope.Views = {
+        Display: "Display.html",
+        Error: "Error.html",
+        Blank: "Blank.html"
+    };
+
+    $scope.CurrentView = $scope.Views.Blank;
+
     $scope.ConvertHandler = function() {
         ApiService.Convert($scope.Amount, $scope.Name, function(response) {
             $scope.ResultAmount = response.data.Text;
             $scope.ResultName = response.data.Name;
-
-            $scope.ErrorMessage = "";
+            $scope.CurrentView = $scope.Views.Display;
         }, function(response) {
             $scope.ErrorMessage = "Conversion failed.";
+            $scope.CurrentView = $scope.Views.Error;
         });
     };
 }]);
